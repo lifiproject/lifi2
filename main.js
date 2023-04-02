19e3d150dcbfc7ded1e0fe567609acf0ee2c535e
lifiproject <129190408+lifiproject@users.noreply.github.com>
2023-04-02 15:01:57 +0530

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-const nav_btn = document.querySelector("#mobilenav");
-const header = document.querySelector(".header")
-
-const toggleNavbar = () => {
-  header.classList.toggle('active');
-};
-
-nav_btn.addEventListener('click', () => toggleNavbar());
-
