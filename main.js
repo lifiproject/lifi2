244963fc56ba10f6deb991b09cab309a349391b8
lifiproject <129190408+lifiproject@users.noreply.github.com>
2023-04-05 03:47:14 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,32 +1,19 @@
-// const nav_btn = document.querySelector("#mobilenav");
-// const header = document.querySelector(".header")
-
-// const toggleNavbar = () => {
-//   header.classList.toggle('active');
-// };
-
-// nav_btn.addEventListener('click', () => toggleNavbar());
-
 const nav_btn = document.querySelector("#mobilenav");
 const header = document.querySelector(".header");
 const navLinks = document.querySelectorAll('.navlist');
-
 const toggleNavbar = () => {
   header.classList.toggle('active');
 };
-
 navLinks.forEach(link => {
   link.addEventListener('click', () => {
     toggleNavbar();
   });
 });
-
 nav_btn.addEventListener('click', () => toggleNavbar());
 
 
 
 var links = document.getElementsByClassName('changeable');
-
 function changeColor(e) {
   // links[0].style.color = '#1F2833';
   // Set all elements to blue
@@ -37,7 +24,21 @@ function changeColor(e) {
     // Set the clicked element to red
     e.target.style.color = '#66FCF1';
 }
-
 for (var i = 0; i < links.length; i++) {
     links[i].addEventListener('click', changeColor);
 }
+
+// Add a delay before starting the animation for each element
+setTimeout(function() {
+  document.querySelector('h5').style.animationDelay = '0.5s';
+  document.querySelector('h5').style.opacity = 1;
+}, 500); // 1s delay for h5 element
+
+setTimeout(function() {
+  document.querySelector('h1').style.opacity = 1;
+}, 500);
+
+setTimeout(function() {
+  document.querySelector('h3').style.animationDelay = '0.7s';
+  document.querySelector('h3').style.opacity = 1;
+}, 700); // 3s delay for h3 element
