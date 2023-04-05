0e595fa1be257e6704625fe270092835a4e438cf
lifiproject <129190408+lifiproject@users.noreply.github.com>
2023-04-05 18:10:14 +0530

    Update main.js

--- main.js
+++ main.js
@@ -28,18 +28,7 @@ for (var i = 0; i < links.length; i++) {
     links[i].addEventListener('click', changeColor);
 }
 
-// Add a delay before starting the animation for each element
-setTimeout(function() {
-  document.querySelector('h5').style.animationDelay = '1s';
-  document.querySelector('h5').style.opacity = 1;
-}, 1000); // 1s delay for h5 element
-
 setTimeout(function() {
   // document.querySelector('h5').style.animationDelay = '2s';
   document.querySelector('h1').style.opacity = 1;
 }, 500);
-
-setTimeout(function() {
-  document.querySelector('h3').style.animationDelay = '5s';
-  document.querySelector('h3').style.opacity = 1;
-}, 6500); // 3s delay for h3 element
